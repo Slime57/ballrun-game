758b1164be5ae1b7e17f4ca1f615f06a560ba2e8
JAXOB <450328@bsd48.org>
2018-04-30 08:42:56 -0700

    Obstacle script
    Makes the player's movement disable when touching obstacles that are tagged "obstacle"

---
+++ BallRun/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	public Movement movement;
+
+	void OnCollisionEnter (Collision collisionInfo){
+
+		if (collisionInfo.collider.tag == "Obstacle") {
+			movement.enabled = false;
+		}
+			
+	}
+}
