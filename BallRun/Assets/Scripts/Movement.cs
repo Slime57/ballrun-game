1fc27687e997a7fbd5cc2736933ba5652bebec95
JAXOB <450328@bsd48.org>
2018-05-04 08:12:49 -0700

    Changed Movement Script
    Now the cube moves forward and backward with W and S, so it's entirely player controlled.

--- BallRun/Assets/Scripts/Movement.cs
+++ BallRun/Assets/Scripts/Movement.cs
@@ -6,11 +6,11 @@ public class Movement : MonoBehaviour {
 
 	public Rigidbody rb;
 
-	public float moveforce = 800;
+	public float moveforce = 800f;
 
 	void FixedUpdate () {
 
-		rb.AddForce (0, 0, -moveforce * Time.deltaTime);
+
 
 		if(Input.GetKey("a")){
 			rb.AddForce(moveforce * Time.deltaTime, 0, 0);
@@ -20,6 +20,15 @@ public class Movement : MonoBehaviour {
 			rb.AddForce(-moveforce * Time.deltaTime, 0, 0);
 		}
 			
+		if(Input.GetKey("w")){
+			rb.AddForce(0, 0, -moveforce * Time.deltaTime);
+		}
+
+		if(Input.GetKey("s")){
+			rb.AddForce(0, 0, moveforce * Time.deltaTime);
+		}
+
+
 		if (rb.position.y < -2) {
 			FindObjectOfType<GameManager> ().Endgame ();
 		}
