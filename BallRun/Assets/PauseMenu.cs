36badb154121f54db50b25c772963e5a93494db5
JAXOB <450328@bsd48.org>
2018-05-08 09:04:47 -0700

    Pause Menu added
    Buttons don't do anything (yet)

--- BallRun/Assets/PauseMenu.cs
+++ BallRun/Assets/PauseMenu.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public static bool GamePaused = false;
+
+	public GameObject pauseMenuUI;
+
+	void Update(){
 		
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (GamePaused) {
+				Resume ();
+			}else{
+				Pause ();
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Resume(){
+		pauseMenuUI.SetActive (false);
+		Time.timeScale = 1f;
+		GamePaused = false;
+	}
+
+	void Pause(){
+		pauseMenuUI.SetActive (true);
+		Time.timeScale = 0f;
+		GamePaused = true;
 	}
 }
