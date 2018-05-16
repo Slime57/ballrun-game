7ce5944b4f85e708633109434317986d7db42cb5
JAXOB <450328@bsd48.org>
2018-05-16 08:37:42 -0700

    Made a Finish
    Probably not perfect right now.

---
+++ BallRun/Assets/Scripts/FinishScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishScript : MonoBehaviour {
+
+	public float finishDelay = 3f;
+
+	void OnCollisionEnter(){
+		Invoke ("FinishLevel", finishDelay);
+	}
+
+
+	void FinishLevel(){
+		SceneManager.LoadScene ("LevelSelection");
+	}
+
+
+}
