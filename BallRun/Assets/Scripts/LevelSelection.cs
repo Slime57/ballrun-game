a1415c9ba3fbb0d8177fb20a0625f18bea34255f
JAXOB <450328@bsd48.org>
2018-05-22 09:00:46 -0700

    Fixed up variable problems

--- BallRun/Assets/Scripts/LevelSelection.cs
+++ BallRun/Assets/Scripts/LevelSelection.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour {
-	public int LevelPoints = 0;
-
+	
 
 	public void PlayLevel1(){
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 	public void PlayLevel2(){
-		if(LevelPoints >= 1){
+		if(ShowVariable.LevelPoints >= 1){
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
 		}
 
 	}
 	public void PlayLevel3(){
-		if(LevelPoints >= 2){
+		if(ShowVariable.LevelPoints >= 2){
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
 		}
 	}
 	public void PlayLevel4(){
-		if(LevelPoints >= 3){
+		if(ShowVariable.LevelPoints >= 3){
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
 		}
 	}
