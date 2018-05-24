c6e913faa934a553569af1619c41ad5c337432e3
VIXEK <403261@bsd48.org>
2018-05-24 08:46:14 -0700

    Added a script to debug the locks

---
+++ BallRun/Assets/Scripts/LocksScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocksScript : MonoBehaviour {
+
+	public GameObject objectToDisable;
+
+	// Use this for initialization
+	void Start () {
+		if( ShowVariable.LevelPoints >= 1){
+			objectToDisable.SetActive (false);
+	}
+}
