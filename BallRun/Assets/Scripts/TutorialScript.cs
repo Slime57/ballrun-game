028e2dc0ee2d446f0f3f9565992bba40b520a22b
JAXOB <450328@bsd48.org>
2018-05-29 09:04:13 -0700

    Finished a Working Tutorial Script

--- BallRun/Assets/Scripts/TutorialScript.cs
+++ BallRun/Assets/Scripts/TutorialScript.cs
@@ -5,13 +5,18 @@ using UnityEngine.UI;
 
 public class TutorialScript : MonoBehaviour {
 
+	public Rigidbody rb;
+
 
 	public Text tutorialtext;
 	private string changingtext = "Welcome to CubeRun!";
 
-	void Start(){
-		tutorialtext.text = "Computer:" + changingtext;
+
+	void Update(){
+		tutorialtext.text = "Computer: " + changingtext;
+			
+		if(rb.position.z <= -20f) {
+			changingtext = "You're objective is to complete the levels";
+		}
 	}
-
-
 }
