9c9860dbd367d69ac1aec9f7637ddcdd757a6153
JAXOB <450328@bsd48.org>
2018-05-29 08:21:49 -0700

    Worked on Tutorial and Level Fixes

---
+++ BallRun/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour {
+
+
+	public Text tutorialtext;
+	private string changingtext = "Welcome to CubeRun!";
+
+	void Start(){
+		tutorialtext.text = "Computer:" + changingtext;
+	}
+
+
+}
