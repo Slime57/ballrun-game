38c6365121d41ce3a55e4c80e6bba1a51ec437de
VIXEK <403261@bsd48.org>
2018-05-29 08:32:05 -0700

    fixed the levelSelectionScript

--- BallRun/Assets/Scripts/LevelSelection.cs
+++ BallRun/Assets/Scripts/LevelSelection.cs
@@ -7,22 +7,24 @@ public class LevelSelection : MonoBehaviour {
 	
 
 	public void PlayLevel1(){
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		if(ShowVariable.LevelPoints == 0){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+		}
 	}
 
 	public void PlayLevel2(){
-		if(ShowVariable.LevelPoints = 1){
+		if(ShowVariable.LevelPoints == 1){
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
 		}
 
 	}
 	public void PlayLevel3(){
-		if(ShowVariable.LevelPoints = 2){
+		if(ShowVariable.LevelPoints == 2){
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
 		}
 	}
 	public void PlayLevel4(){
-		if(ShowVariable.LevelPoints = 3){
+		if(ShowVariable.LevelPoints == 3){
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
 		}
 	}
