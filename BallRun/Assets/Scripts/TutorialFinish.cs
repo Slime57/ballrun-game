21fb4b23482edb06abe1306853c65788274531ce
JAXOB <450328@bsd48.org>
2018-05-31 08:44:11 -0700

    Tutorial Finished!

---
+++ BallRun/Assets/Scripts/TutorialFinish.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialFinish : MonoBehaviour {
+
+	public float finishDelay = 3f;
+
+	void OnTriggerEnter(){
+		Invoke ("FinishLevel", finishDelay);
+	}
+
+
+	public void FinishLevel(){
+		SceneManager.LoadScene ("LevelSelection");
+
+	}
+}
--- BallRun/Assets/Scripts/TutorialScript.cs
+++ BallRun/Assets/Scripts/TutorialScript.cs
@@ -9,7 +9,7 @@ public class TutorialScript : MonoBehaviour {
 
 
 	public Text tutorialtext;
-	private string changingtext = "Welcome to CubeRun!";
+	private string changingtext = "Welcome to CubeRun! (Hint: Use W, A, S, and D to move!)";
 
 
 	void Update(){
@@ -17,6 +17,36 @@ public class TutorialScript : MonoBehaviour {
 			
 		if(rb.position.z <= -20f) {
 			changingtext = "You're objective is to complete the levels";
+		}
+		if(rb.position.z <= -40f) {
+			changingtext = "However, there are obstacles. There's a bumper to your left, and a spike to your right";
+		}
+		if(rb.position.z <= -50f) {
+			changingtext = "Good job! I was just going to mention, you should NOT touch those!";
+		}
+		if(rb.position.z <= -70f) {
+			changingtext = "To climb a ramp, move forward in it's direction, then swerve side to side.";
+		}
+		if(rb.position.z <= -84f) {
+			changingtext = "Great job! You're learning fast.";
+		}
+		if(rb.position.z <= -87f) {
+			changingtext = "The last obstacle is the tube. Be watchful of your speed!";
+		}
+		if(rb.position.z <= -112f) {
+			changingtext = "Were you surprised? The last obstacle is actually falling!";
+		}
+		if(rb.position.z <= -118f) {
+			changingtext = "Let's see if you can beat this!";
+		}
+		if(rb.position.z <= -140f) {
+			changingtext = "You've done great! I think you're ready.";
+		}
+		if(rb.position.z <= -160f) {
+			changingtext = "This is a goal. Make it through the hole to win!";
+		}
+		if(rb.position.z <= -192f) {
+			changingtext = "Congradulations! Have fun!";
 		}
 	}
 }
